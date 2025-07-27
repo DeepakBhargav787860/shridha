663ac2400958c553b6ca70ac2bb3b836a37e2d0f
DeepakBhargav787860 <Deepak.Bhargav787860@gmail.com>
2025-07-27 14:37:07 +0530

    may be be

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: '/shridha/', // 👈 must match your GitHub repo name
   plugins: [react()],
+    server:{
+    host:'127.0.0.1'
+  }
 })
 
 // https://vitejs.dev/config/
