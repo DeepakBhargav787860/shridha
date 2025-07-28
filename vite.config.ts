a5ab73c4c042253447827a4fcb8cb18bb95f1249
DeepakBhargav787860 <Deepak.Bhargav787860@gmail.com>
2025-07-28 10:38:13 +0530

    check working

--- vite.config.ts
+++ vite.config.ts
@@ -1,46 +1,8 @@
-import path from "path";
-import {defineConfig, splitVendorChunkPlugin} from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.ts
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/shridha/', // 👈 Your repo name
   plugins: [react()],
-   resolve: {
-    alias: {
-      '@': path.resolve('./src')
-    },
-  }
 });
-
-// https://vitejs.dev/config/
-
-// export default defineConfig({
-//   plugins: [react()],
-//   server:{
-//     host:'127.0.0.1'
-//   }
-// })
-
-
-// export default defineConfig({
-
-//   //un-comment for Production debugging
-//   // build: {
-//   //   emptyOutDir: true,
-//   //   manifest: true,
-//   //   minify: false,
-//   //   sourcemap: true,
-//   //   ssr: false,
-//   // },
-
-//   resolve: {
-//     alias: {
-//       '@': path.resolve('./src')
-//     },
-
-//   },
-//   plugins: [react(),  splitVendorChunkPlugin()],
-//     server:{
-//     host:'127.0.0.1'
-//   },
-
-// });
